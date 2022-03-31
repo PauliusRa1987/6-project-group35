7d9baa8fda963c58c3e75dbd54e8b8bbd7b53c96
PauliusRa <buvizz@gmail.com>
2022-03-31 11:43:58 +0300

    funkcijos

--- js/01-variables.js
+++ js/01-variables.js
@@ -2,3 +2,4 @@ const skaicius = 123;
 console.log(123);
 console.log("Laba diena, petrai");
 console.log(skaicius);
+
---
+++ js/05-if.js
@@ -0,0 +1,24 @@
+/* 
+IF
+
+Sablonai:
+- if () {}
+- if () {} else {}
+
+
+Palyginimo operatoriai:
+- visi <,  >, <=, >=, ==, !=, ===, !== 
+- naudotini <,  >, <=, >=, ===, !== 
+-nenaudoti ==, !=
+*/
+
+
+const a = 7;
+const b = 5;
+
+if (a > b) {
+    console.log('A yra daugiau uz B');
+}else {
+    console.log('A nera daugiau uz B');
+}
+
